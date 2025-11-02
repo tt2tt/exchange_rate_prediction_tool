05f88d9c5f0eeef63bbedbc171e757cbc346d538
tt <groveleffect@gmail.com>
2025-11-02 14:00:32 +0900

    バックテストモジュールと検証テストを追加

--- core/model/__init__.py
+++ core/model/__init__.py
@@ -1,10 +1,15 @@
 """モデル学習関連の公開API。"""
 
+from .backtester import BacktestConfig, BacktestMetrics, BacktestResult, Backtester
 from .evaluation import EvaluationConfig, EvaluationResult, ModelEvaluator
 from .logistic_regression_trainer import LogisticRegressionTrainer, TrainingConfig, TrainingResult
 from .predictor import PredictionResult, Predictor
 
 __all__ = [
+    "BacktestConfig",
+    "BacktestMetrics",
+    "BacktestResult",
+    "Backtester",
     "EvaluationConfig",
     "EvaluationResult",
     "ModelEvaluator",
